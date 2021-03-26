c1eba05da124ea0daa78a24fd60ad721de37da54
Natan Souza <42328139+natan1506@users.noreply.github.com>
2021-03-26 10:32:43 -0300

    correções pontuais

--- src/Buttons.js
+++ src/Buttons.js
@@ -8,7 +8,7 @@ const Buttons = {
 
     document.body.innerHTML =  `
       <div class="d-flex flex-column align-items-center">
-        <img src="../assets/logo-ml.svg" alt="MarcaLaser" width="250"/>
+        <img src="assets/logo-ml.svg" alt="MarcaLaser" width="250"/>
         <div>
           <button type="button" class="btn btn-sm btn-outline-primary m-1" id="buttonTagInitial">Etiqueta para Pedido</button>
           <button type="button" class="btn btn-sm btn-outline-primary m-1"
